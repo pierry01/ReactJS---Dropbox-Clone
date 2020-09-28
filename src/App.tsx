1fd41c8759325ab1126ea0375336f480de55e95b
Jean Mendonça <pierrybm@gmail.com>
2020-09-28 19:47:06 +0000

    Building the HOME PAGE

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,45 @@
 import React from 'react';
 
-function App() {
+import GlobalStyles from './styles/GlobalStyles'
+import Section from './components/Section'
+
+import data from './data'
+
+const App = () => {
   return (
-    <div className="App">
-      ...
-    </div>
+    <>
+      <Section
+        variant="blue"
+        title={data[0].title}
+        description={data[0].description}
+      />
+      
+      <Section
+        variant="beige"
+        title={data[1].title}
+        description={data[1].description}
+      />
+      
+      <Section
+        variant="blue"
+        title={data[2].title}
+        description={data[2].description}
+      />
+      
+      <Section
+        variant="white"
+        title={data[3].title}
+        description={data[3].description}
+      />
+      
+      <Section
+        variant="black"
+        title={data[4].title}
+        description={data[4].description}
+      />
+      
+      <GlobalStyles />
+    </>
   )
 }
 
